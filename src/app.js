a52406904f563935617292491cf905c0ffc6a53c
Crystal Lin <crystal.lin.cml@gmail.com>
2020-10-04 11:28:17 -0700

    file structure that runs with npm start

--- src/app.js
+++ src/app.js
@@ -9,17 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/v1', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 200 OK status*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return res.status(200).json({ message: 'Welcome to Express API template' });
 });
 
 module.exports = router;
