b433fbd7c733568884d521e18a8559ca3580c88a
Crystal Lin <crystal.lin.cml@gmail.com>
2020-10-04 13:56:55 -0700

    environment variables set up

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../config';
 
 const indexRouter = express.Router();
 
 /* GET home page. 200 OK status */
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
