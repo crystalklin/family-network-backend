7569122e3e526122c30868f912a1e429f8a7fd8f
Crystal Lin <crystal.lin.cml@gmail.com>
2020-10-04 13:40:09 -0700

    linting, eslint & prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET home page. 200 OK status*/
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
-});
+const router = express.Router();
+
+/* GET home page. 200 OK status */
+router.get('/', (req, res, next) => res.status(200).json({ message: 'Welcome to Express API template' }));
 
 module.exports = router;
